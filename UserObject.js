a397e839ada45754f618b2f0d593c645026a4ed6
Lavi Raghav <lavi.raghav@users.noreply.github.com>
2026-08-14 09:55:05 +0530

    Add current files

---
+++ UserObject.js
@@ -0,0 +1,20 @@
+const employee={
+    empId:2,
+    name:'Ram',
+    designation:'developer',
+    salary:30000,
+    techstack:[{
+        plang:"java",
+        server:'Apache Tomcat',
+        database:'MySQL',
+        database2:'MongoDB'
+    },
+{
+    plang:'python',
+    server:'Django',
+    database:'PostgreSQL',
+    database2:'SQLite'
+
+}]
+    }
+    console.log(employee)
